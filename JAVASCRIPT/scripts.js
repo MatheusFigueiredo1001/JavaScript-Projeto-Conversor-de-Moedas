84ff7ef6bf524c9edd52c46ea7939b79db366c99
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2024-12-03 12:27:10 -0300

    novos projetos

---
+++ JAVASCRIPT/scripts.js
@@ -0,0 +1,39 @@
+/*
+document --> HTML
+
+getElementById --> Trás um elemento pelo ID
+getElementByClassName --> Trás todos os elementos com essa classe
+getElementByTagName --> Trás todos os elementos com essa tag
+getElementByName --> Trás todos os elementos com esse name
+
+querySelector --> Trás um elemento, o primeiro que encontrar
+querySelectorAll --> Trás todos os elementos que encontrar
+
+Alternado e acessando textos
+
+textContent --> Pega todo conteúdo
+innerText --> Pega apenas textos
+innerHTML --> Permite adicionar HTML e texto
+*/
+
+/*const input = document.getElementById("main-input")*/
+
+/*const paragraph = document.getElementsByClassName("paragraph-js")*/
+
+/*const tag = document.getElementsByTagName("p")*/
+
+/*const name = document.getElementsByName("nome-completo")*/
+
+/*const element = document.querySelector("#main-input")
+element.placeholder = "Mudei o texto"*/
+
+/*const element = document.querySelector(".paragraph.js")
+
+console.log(element) // Só HTML
+console.log(element) // LEVA EM CONTA O CSS
+console.log(element) // Traz tudo*/
+
+const button= document.querySelector(".main-button")
+
+console.log(button)
+
