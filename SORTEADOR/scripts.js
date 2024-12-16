63cab18de94056fd07b525b35d2a65158613e3b3
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2024-12-16 14:48:36 -0300

    Atualizações

--- SORTEADOR/scripts.js
+++ SORTEADOR/scripts.js
@@ -1,17 +1,20 @@
 function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
+    const quantity = parseInt(document.querySelector(".input-quantity").value)
 
     if (min >= max) {
         alert("O valor mínimo tem que ser menor que o valor máximo")
     }
 
     else {
-
-        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        let results = []
+        for (let i = 0; i < quantity; i++) {
+            const result = Math.floor(Math.random() * (max - min + 1)) + min;
+            results.push(result)
+        }
         const resultDisplay = document.querySelector(".result-display")
-
-        resultDisplay.textContent = `O número sorteado é ${result}`
+        resultDisplay.textContent = `Os números sorteados são ${results.join(", ")}`
     }
 
 }
